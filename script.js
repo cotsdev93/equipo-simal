a2fa77bb766622c067c181ca1f10abcc28ea3232
cotsdev93 <cotsdev93@gmail.com>
2025-06-02 15:58:19 -0300

    nav mobile version

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("funciona")
+
+const menu = document.querySelector(".fa-bars")
+
+menu.addEventListener("click",() =>{
+    const nav2 = document.querySelector(".nav2")
+    console.log("va")
+    nav2.classList.toggle("animation")
+})
+
