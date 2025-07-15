0e8dcfa7dceac08261961d2552072d6287a9b1a6
cotsdev93 <cotsdev93@gmail.com>
2025-07-15 11:08:58 -0300

    prueba

--- script.js
+++ script.js
@@ -1,42 +1,16 @@
-console.log("funciona");
+/////////////////////////////////////////////////////// MENU
 
-const menu = document.querySelector(".fa-bars");
+const puchinball = document.querySelector(".puchinball");
+const cart = document.querySelector(".cart");
+const comunidad = document.querySelector(".comunidad");
+const tienda = document.querySelector(".tienda");
 
-menu.addEventListener("click", () => {
-  const nav2 = document.querySelector(".nav2");
-  console.log("va");
-  nav2.classList.toggle("animation");
+puchinball.addEventListener("click", () => {
+  cart.classList.toggle("toggle");
+  setTimeout(() => {
+    tienda.classList.toggle("toggle");
+  }, 150);
+  setTimeout(() => {
+    comunidad.classList.toggle("toggle");
+  }, 300);
 });
-
-class Products {
-  constructor() {
-    this.products = [];
-    this.cargarRegistros();
-  }
-
-  async cargarRegistros() {
-    const resultado = await fetch("data/products.JSON");
-    this.products = await resultado.json();
-    cargarProducts(this.products);
-    console.log(this.products);
-  }
-}
-
-function cargarProducts(products) {
-  const productsDiv = document.getElementById("products");
-  for (let product of products) {
-    productsDiv.innerHTML += `
-        <div class="productContainer">
-          <div class="imgContainer">
-            <img src="${product.img}" />
-          </div>
-          <div class="productSpecs">
-            <p>${product.name}</p>
-            <p>${product.price}</p>
-          </div>
-        </div>
-      `;
-  }
-}
-
-const products = new Products();
